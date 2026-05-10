9796e57fc589f3733d6ac42d3a27463b3f28d521
Haroon Amin <aminharoon699@gmail.com>
2026-05-10 11:01:05 +0530

    frontend init complete

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -19,6 +19,8 @@ import orderRoutes from "./routes/order.routes.js"
 
 const app = express();
 app.use(express.json());
+app.use(express.static("../public"));
+
 app.use(morgan("dev"));
 app.use(cors({ origin: ["http://localhost:5173", "*"], credentials: true, }));
 app.use(cookieParser());
